f840f8bd407238b8f28435385b4b5e84d431ef13
PedroYokada <pedro_hlyokada@outlook.com>
2024-08-17 22:46:15 -0300

    Exercicios de Python - exercicios novos 17/08 - 22:46

--- EstruturaSequencial/loja_tinta.py
+++ EstruturaSequencial/loja_tinta.py
@@ -2,4 +2,12 @@
 # Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, 
 # que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 
+metros_quadrados = float(input("Insira a quantidade de metros quadrados: "))
 
+litros = float(metros_quadrados / 3)
+
+latas = float(litros / 18)
+
+preco_final = float(latas * 80.00)
+
+print(f"O preço total é de da tinta é de: {preco_final:.2f}", "R$")
