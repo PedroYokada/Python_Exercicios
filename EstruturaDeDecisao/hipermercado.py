c304c49065fc0235cafdb31d785cc6b4ec35befb
PedroYokada <pedro_hlyokada@outlook.com>
2024-08-26 15:26:56 -0300

    Exercicio hipermercado - 26/08 - 15:28

--- EstruturaDeDecisao/hipermercado.py
+++ EstruturaDeDecisao/hipermercado.py
@@ -16,7 +16,7 @@ alcatra_mais5 = 6.80
 picanha_ate5 = 6.90
 picanha_mais5 = 7.80
 
-tipo_carne = input("Escolha um tipo de carne, F:Filé Duplo/A:Alcatra/P:Picanha")
+tipo_carne = input("Escolha um tipo de carne, F:Filé Duplo/A:Alcatra/P:Picanha: ").strip().upper()
 
 match tipo_carne:
     case "F":
@@ -25,17 +25,82 @@ match tipo_carne:
           compra = qnt_carne * file_ate5
         else:
           compra = qnt_carne * file_mais5
+        
+        forma_pagamento = input("Qual a forma de pagamento ? C-Cartão/O-Outros: ").strip().upper()
+        
+        if forma_pagamento == "C":
+          compra = compra - (compra * 0.05)
+          print(f"O tipo da carne é: Filé duplo.")
+          print(f"A quantidade de carne foi de: {qnt_carne:.2f}Kg")
+          print(f"A forma de pagamento foi: Cartão Tabajara.")
+          print(f"O desconto da compra é de 5%.")
+          print(f"O valor total da compra é de: {compra:.2f}R$")
+          exit()
+        elif forma_pagamento == "O":
+          print(f"O tipo da carne é: Filé duplo.")
+          print(f"A quantidade de carne foi de: {qnt_carne:.2f}Kg")
+          print(f"A forma de pagamento foi: Outros.")
+          print(f"Não tem desconto, é exclusivo do pagamento em cartão.")
+          print(f"O valor total da compra é de: {compra:.2f}R$")
+          exit()
+        else:
+          print("Forma de pagamento inválida.")
+          exit()
     case "A":
         qnt_carne = float(input("Digite a quantidade em Kg: "))
         if qnt_carne <= 5:
           compra = qnt_carne * alcatra_ate5
         else:
           compra = qnt_carne * alcatra_mais5
+          
+        forma_pagamento = input("Qual a forma de pagamento ? C-Cartão/O-Outros: ").strip().upper()
+        
+        if forma_pagamento == "C":
+          compra = compra - (compra * 0.05)
+          print(f"O tipo da carne é: Alcatra.")
+          print(f"A quantidade de carne foi de: {qnt_carne:.2f}Kg")
+          print(f"A forma de pagamento foi: Cartão Tabajara.")
+          print(f"O desconto da compra é de 5%.")
+          print(f"O valor total da compra é de: {compra:.2f}R$")
+          exit()
+        elif forma_pagamento == "O":
+          print(f"O tipo da carne é: Alcatra.")
+          print(f"A quantidade de carne foi de: {qnt_carne:.2f}Kg")
+          print(f"A forma de pagamento foi: Outros.")
+          print(f"Não tem desconto, é exclusivo do pagamento em cartão.")
+          print(f"O valor total da compra é de: {compra:.2f}R$")
+          exit()
+        else:
+          print("Forma de pagamento inválida.")
+          exit()
+         
     case "P":
         qnt_carne = float(input("Digite a quantidade em Kg: "))
         if qnt_carne <= 5:
           compra = qnt_carne * picanha_ate5
         else:
           compra = qnt_carne * picanha_mais5
+        
+        forma_pagamento = input("Qual a forma de pagamento ? C-Cartão/O-Outros: ").strip().upper()
+        
+        if forma_pagamento == "C":
+          compra = compra - (compra * 0.05)
+          print(f"O tipo da carne é: Picanha.")
+          print(f"A quantidade de carne foi de: {qnt_carne:.2f}Kg")
+          print(f"A forma de pagamento foi: Cartão Tabajara.")
+          print(f"O desconto da compra é de 5%.")
+          print(f"O valor total da compra é de: {compra:.2f}R$")
+          exit()
+        elif forma_pagamento == "O":
+          print(f"O tipo da carne é: Picanha.")
+          print(f"A quantidade de carne foi de: {qnt_carne:.2f}Kg")
+          print(f"A forma de pagamento foi: Outros.")
+          print(f"Não tem desconto, é exclusivo do pagamento em cartão.")
+          print(f"O valor total da compra é de: {compra:.2f}R$")
+          exit()
+        else:
+          print("Forma de pagamento inválida.")
+          exit()
     case _:
         print("Tipo de carne inválido.")
+        exit()
